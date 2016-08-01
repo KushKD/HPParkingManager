91501a462eb92bd600204c29042c94d184252ca1
kuush <kushkumardhawan@gmail.com>
2016-08-01 15:37:12 +0530

    -- 4.1.4 Ready to Launch

--- app/src/main/java/Utils/EConstants.java
+++ app/src/main/java/Utils/EConstants.java
@@ -7,10 +7,10 @@ public class EConstants  {
 
     public static final String URL_GENERIC_Testing = "http://192.168.0.171/HPParking/HPParking.svc/getparkingLocation_JSON/";
     public static final String URL_GENERIC_Production = "http://hpparking.hp.gov.in/HPParking.svc/getparkingLocation_JSON/";
-   // public static final String Production_URL = "http://hpparking.hp.gov.in/HPParking.svc/"; //Main Server
+    public static final String Production_URL = "http://hpparking.hp.gov.in/HPParking.svc/"; //Main Server
    // public static final String Production_URL = "http://192.168.0.197:7002/HPParking.svc/";  //Kush PC
 //    public static final String Production_URL = "http://10.126.161.38/HPParking/HPParking.svc/";  //AMIT PC
-    public static final String Production_URL = "http://192.168.0.171/HPParking/HPParking.svc/";  //Anil Sir Room
+ //   public static final String Production_URL = "http://192.168.0.171/HPParking/HPParking.svc/";  //Anil Sir Room
     public static final String PREF_NAME = "Manager_Console";
     public static String ParkingID_Task = null;
 }
